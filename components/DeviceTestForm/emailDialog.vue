f9b88ab6ab7c4b4ac90deb52913ce83fefdf2e5c
cekun <cekung@gmail.com>
2021-09-13 10:30:26 +0800

    设置邮件通知对话框

--- components/DeviceTestForm/emailDialog.vue
+++ components/DeviceTestForm/emailDialog.vue
@@ -1,28 +1,61 @@
 <template>
   <el-dialog
-    title="选择被测设备"
+    title="设置邮件通知人"
     :visible.sync="show"
     :destroy-on-close="true"
     :close-on-click-modal="false"
     @close="$emit('close')"
   >
     <el-table
-      :data="list" border highlight-current-row
+      ref="table"
+      :data="list" border
+      @selection-change="handleSelectionChange"
     >
-      <el-table-column prop="deviceId" label="设备ID" align="center" />
-      <el-table-column prop="deviceName" label="设备名称" align="center" />
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column prop="email" label="邮箱地址" align="center" />
+      <el-table-column prop="emailUserName" label="名称" align="center" />
     </el-table>
+    <span slot="footer">
+      <el-button @click="$emit('close')">取消</el-button>
+      <el-button type="primary" @click="$emit('confirm', emails)" :disabled="!emails.length">确认</el-button>
+    </span>
   </el-dialog>
 </template>
 
 <script>
 export default {
+  props: {
+    visible: Boolean
+  },
   data() {
     return {
+      show: false,
       list: [
-        { emialId: '123323123123', emailName: 'nsd@qq.com' },
-        { emialId: '234234234233', emailName: '1nassd@163.com' }
-      ]
+        { emailId: '112', email: '123@qq.com', emailUserName: '桂松' },
+        { emailId: '212', email: 'evsaw@163.com', emailUserName: '瘪三' }
+      ],
+      emails: []
+    }
+  },
+  watch: {
+    visible(val) {
+      this.show = val
+      if(!val)
+        this.emails = []
+      else {
+        // 回显选中
+        var ids = this.emails.map(item => item.emailId)
+        this.list.forEach(row => {
+          if(ids.indexOf(row.emailId) > -1) {
+            this.$refs.table.toggleRowSelection(row)
+          }
+        })
+      }
+    }
+  },
+  methods: {
+    handleSelectionChange(val) {
+      this.emails = val
     }
   },
 };

df5f9117ecdc74a7858219f94525b3d7f7f2e1a8
cekun <cekung@gmail.com>
2021-09-16 16:52:50 +0800

    svg-spite-loader创建svg-icon组件

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,45 +1,41 @@
+const { resolve } = require('path')
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: '自动化测试',
+    title: "自动化测试",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    'element-ui/lib/theme-chalk/index.css',
-    '@/assets/css/index'
-  ],
+  css: ["element-ui/lib/theme-chalk/index.css", "@/assets/css/index"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/element-ui',
-    '@/plugins/axios'
+    "@/plugins/element-ui",
+    "@/plugins/axios",
+    '@/plugins/svg-icon'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    '@nuxtjs/proxy'
+    "@nuxtjs/axios",
+    "@nuxtjs/proxy"
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -48,14 +44,29 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: [/^element-ui/],
+    extend(config, context) {
+      // 排除 nuxt 原配置的影响,Nuxt 默认有vue-loader,会处理svg,img等
+      // 找到匹配.svg的规则,然后将存放svg文件的目录排除
+      const svgRule = config.module.rules.find(rule => rule.test.test(".svg"));
+      svgRule.exclude = [resolve(__dirname, "icons/svg")];
+
+      //添加loader规则
+      config.module.rules.push({
+        test: /\.svg$/, //匹配.svg
+        include: [resolve(__dirname, "icons/svg")], //将存放svg的目录加入到loader处理目录
+        use: [
+          { loader: "svg-sprite-loader", options: { symbolId: "icon-[name]" } }
+        ]
+      });
+    }
   },
   proxy: {
     "/api/": {
-      target: 'http://127.0.0.1:7001',
+      target: "http://10.117.4.205:8010/auto-device-test",
       secure: false,
       pathRewrite: {
-        '^/api': ""
+        "^/api": ""
       }
     }
   }
-}
+};
---
+++ plugins/svg-icon.js
@@ -0,0 +1,4 @@
+
+const req = require.context('@/icons/svg', false, /\.svg$/)
+const requireAll = requireContext => requireContext.keys().map(requireContext)
+requireAll(req)
